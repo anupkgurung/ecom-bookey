35b982199af0b3933625cc4d4de0ad2c0461320a
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-03-23 21:05:13 +0530

    added extras

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,66 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Architecture",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "Arts & Photography",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "Biographies & Memoirs",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Travel & holiday", 
+  },
+  {
+    _id: uuid(),
+    categoryName: "Computer & Internet",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Dictionaries & Language",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Business & Economics",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Fiction",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Self Help & Personal Development",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Technology & Engineering",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Non-fiction",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Body, Mind & Spirit",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Cookery, Food & Wine",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Sports",
+  },
+  {
+    _id: uuid(),
+    categoryName: "History & Humanities",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Children & Teens",
   },
 ];
