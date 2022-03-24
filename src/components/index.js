2472532997c9c5d921ca6de911be36a900d6bf47
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-03-24 17:16:39 +0530

    added index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {Navbar} from "./Navbar/Navbar";
 export {Cateogory} from "./Cateogory/Cateogory";
 export {VerticleProductCard} from "./Product/VerticleProductCard";
+export { Input } from "./Input/Input";
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom"
+import { AuthenticationProvider } from "./context"
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <AuthenticationProvider>
+        <App />
+      </AuthenticationProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
